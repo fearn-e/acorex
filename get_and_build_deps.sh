abf4bd6cc159a9921fefee6c97881eb3e39fd153
Elowyn Fearne <gitfearne@proton.me>
2024-03-10 15:09:42 +0000

    different cmake commands for mac and windows

--- get_and_build_deps.sh
+++ get_and_build_deps.sh
@@ -1,41 +1,50 @@
+currentOS="win"
+# Get OS type
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+        currentOS="mac"
+    elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW), not sure if win32 can even happen?
+        currentOS="win"
+    else
+        # Unknown, cygwin, linux-gnu
+        echo "no official support for this OS, script functionality may not be as expected"
+    fi
+#
+
 # Download repos
     echo "--------------------------------------------------"
     echo "downloading repos to deps-pre-build"
     echo ""
 
-    if [ ! -d "deps-pre-build" ]
-    then
+    if [ ! -d "deps-pre-build" ]; then
         mkdir deps-pre-build
     fi
 
     cd deps-pre-build
 
-    if [ ! -d "flucoma-core" ]
-    then
+    if [ ! -d "flucoma-core" ]; then
         git clone https://github.com/flucoma/flucoma-core
     else
         echo "flucoma-core already exists, skipping download"
     fi
     echo ""
 
-    if [ ! -d "eigen" ]
-    then
+    if [ ! -d "eigen" ]; then
         git clone https://gitlab.com/libeigen/eigen
     else
         echo "eigen already exists, skipping download"
     fi
     echo ""
 
-    if [ ! -d "hisstools_library" ]
-    then
+    if [ ! -d "hisstools_library" ]; then
         git clone --branch header-only https://github.com/alexharker/hisstools_library
     else
         echo "hisstools_library already exists, skipping download"
     fi
     echo ""
 
-    if [ ! -d "memory" ]
-    then
+    if [ ! -d "memory" ]; then
         git clone https://github.com/foonathan/memory
     else
         echo "memory already exists, skipping download"
@@ -48,23 +57,45 @@
     echo "--------------------------------------------------"
     echo "cmaking foonathan memory libs"
     echo ""
-    if [ -d "compiled-memory" ]
-    then
+    if [ -d "compiled-memory" ]; then
         rm -r -f compiled-memory
     fi
     cp -r memory/ compiled-memory/
 
     #build foonathan_memory libs
     cd compiled-memory
-    cmake -DFOONATHAN_MEMORY_BUILD_TESTS=OFF -DFOONATHAN_MEMORY_BUILD_TOOLS=OFF -DFOONATHAN_MEMORY_BUILD_EXAMPLES=OFF .
-    echo ""
-    echo "creating debug lib"
-    echo ""
-    cmake --build . --config Debug
-    echo ""
-    echo "creating release lib"
-    echo ""
-    cmake --build . --config Release
+    echo $currentOS
+
+    if [ "$currentOS" == "win" ]; then
+        cmake -DFOONATHAN_MEMORY_BUILD_TESTS=OFF -DFOONATHAN_MEMORY_BUILD_TOOLS=OFF -DFOONATHAN_MEMORY_BUILD_EXAMPLES=OFF .
+
+        echo ""
+        echo "creating debug lib"
+        echo ""
+
+        cmake --build . --config Debug
+
+        echo ""
+        echo "creating release lib"
+        echo ""
+
+        cmake --build . --config Release
+    elif [ "$currentOS" == "mac" ]; then
+        cmake -DCMAKE_BUILD_TYPE=DEBUG -DFOONATHAN_MEMORY_BUILD_TESTS=OFF -DFOONATHAN_MEMORY_BUILD_TOOLS=OFF -DFOONATHAN_MEMORY_BUILD_EXAMPLES=OFF .
+        cmake -DCMAKE_BUILD_TYPE=RELEASE -DFOONATHAN_MEMORY_BUILD_TESTS=OFF -DFOONATHAN_MEMORY_BUILD_TOOLS=OFF -DFOONATHAN_MEMORY_BUILD_EXAMPLES=OFF .
+        
+        echo ""
+        echo "creating debug lib"
+        echo ""
+
+        cmake --build . --config Debug
+
+        echo ""
+        echo "creating release lib"
+        echo ""
+
+        cmake --build . --config Release
+    fi
 
     cd ../..
 #
@@ -73,8 +104,7 @@
     echo "--------------------------------------------------"
     echo "installing dependencies to deps folder"
     echo ""
-    if [ -d "deps" ]
-    then
+    if [ -d "deps" ]; then
         rm -r -f deps
     fi
     mkdir deps
@@ -95,4 +125,4 @@
 #
 
 echo ""
-echo "finished, acorex project is now ready to build"
+echo "script finished"
