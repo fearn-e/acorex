87dcedc3228ff6678cf3c1b8fc0f80c98bda998e
Elowyn Fearne <gitfearne@proton.me>
2024-03-27 01:23:47 +0000

    change defs to struct
    they're all public anyway

--- src/Interface/InterfaceDefs.h
+++ src/Interface/InterfaceDefs.h
@@ -4,8 +4,7 @@
 
 namespace AcorexInterface {
 
-	class Colors {
-	public:
+	struct Colors {
 		//normal and locked text colour
 		ofColor normalTextColor = { 255, 255, 255, 255 };
 		ofColor lockedTextColor = { 160, 160, 160, 255 };
