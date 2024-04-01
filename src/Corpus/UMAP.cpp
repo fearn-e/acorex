44dd0771640be3972018f1dbf63534463f630740
Elowyn Fearne <gitfearne@proton.me>
2024-04-01 17:06:15 +0100

    UMAP updated to use new dataset

--- src/Corpus/UMAP.cpp
+++ src/Corpus/UMAP.cpp
@@ -3,37 +3,111 @@
 #include "UMAP.h"
 #include "ofLog.h"
 
-bool AcorexCorpus::UMAP::Fit ( fluid::FluidDataSet<std::string, double, 1>& datasetIN, fluid::FluidDataSet<std::string, double, 1>& datasetOUT )
+bool AcorexCorpus::UMAP::Fit ( AcorexCorpus::DataSet& dataset, const AcorexCorpus::ReductionSettings& settings )
 {
     fluid::algorithm::UMAP algorithm;
 
-    datasetOUT = algorithm.train ( datasetIN, 15, 3, 0.1, 200, 0.1 );
+	fluid::FluidDataSet<std::string, double, 1> fluidsetIN ( dataset.currentDimensionCount );
+	fluid::FluidDataSet<std::string, double, 1> fluidsetOUT ( settings.dimensionReductionTarget );
+
+	std::vector<int> filePointLength ( dataset.isTimeAnalysis ? dataset.currentPointCount : 0, false );
+
+	CorpusToFluid ( fluidsetIN, dataset, filePointLength );
+
+	fluidsetOUT = algorithm.train ( fluidsetIN, 15, settings.dimensionReductionTarget, 0.1, settings.maxIterations, 0.1 );
+
+	FluidToCorpus ( dataset, fluidsetOUT, filePointLength, settings.dimensionReductionTarget );
+
+	dataset.isReduction = true;
 
     return true;
 }
 
-bool AcorexCorpus::UMAP::FitOverTime ( fluid::FluidDataSet<std::string, double, 1>& datasetIN, fluid::FluidDataSet<std::string, double, 1>& datasetOUT )
+void AcorexCorpus::UMAP::CorpusToFluid ( fluid::FluidDataSet<std::string, double, 1>& fluidset, const AcorexCorpus::DataSet& dataset, std::vector<int>& filePointLength )
 {
-	fluid::algorithm::UMAP algorithm;
+	if ( dataset.isTimeAnalysis )
+	{	
+		for ( int file = 0; file < dataset.tData.size ( ); file++ )
+		{
+			for ( int timepoint = 0; timepoint < dataset.tData[file].size ( ); timepoint++ )
+			{
+				fluid::RealVector point ( dataset.tData[file][timepoint].size ( ) );
 
-	fluid::FluidDataSet<std::string, double, 1> empty;
-	fluid::FluidDataSet<std::string, double, 1> datasetNoTimeIN ( datasetIN.dims ( ) - 2 );
-	fluid::FluidDataSet<std::string, double, 1> datasetNoTimeOUT ( 3 );
-	fluid::FluidDataSet<std::string, double, 1> datasetTIME ( 2 );
+				for ( int dimension = 0; dimension < dataset.tData[file][timepoint].size ( ); dimension++ )
+				{
+					point[dimension] = dataset.tData[file][timepoint][dimension];
+				}
 
-	mQuery.clear ( );
-	mQuery.addRange ( 0, 2 );
-	mQuery.process ( datasetIN, empty, datasetTIME );
+				fluidset.add ( std::to_string ( file ), point );
+			}
 
-	mQuery.clear ( );
-	mQuery.addRange ( 2, 24 );
-	mQuery.process ( datasetIN, empty, datasetNoTimeIN );
+			filePointLength.push_back ( dataset.tData[file].size ( ) );
+		}
+	}
+	else
+	{
+		for ( int file = 0; file < dataset.sData.size ( ); file++ )
+		{
+			fluid::RealVector point ( dataset.currentDimensionCount );
+			
+			for ( int dimension = 0; dimension < dataset.sData[file].size ( ); dimension++ )
+			{
+				for ( int statistic = 0; statistic < dataset.sData[file][dimension].size ( ); statistic++ )
+				{
+					point[( dimension * DATA_NUM_STATS ) + statistic] = dataset.sData[file][dimension][statistic];
+				}
+			}
 
-	datasetNoTimeOUT = algorithm.train ( datasetNoTimeIN, 15, 3, 0.1, 200, 0.1 );
+			fluidset.add ( std::to_string ( file ), point );
+		}
+	}
+}
 
-	mQuery.clear ( );
-	mQuery.addRange ( 0, 3 );
-	mQuery.process ( datasetNoTimeOUT, datasetTIME, datasetOUT );
+void AcorexCorpus::UMAP::FluidToCorpus ( AcorexCorpus::DataSet& dataset, const fluid::FluidDataSet<std::string, double, 1>& fluidset, const std::vector<int>& filePointLength, const int reducedDimensionCount )
+{
+	if ( dataset.isTimeAnalysis )
+	{
+		dataset.tData.clear ( );
 
-	return true;
+		int fileMarker = 0;
+
+		for ( int filePoint = 0; filePoint < filePointLength.size ( ); filePoint++ )
+		{
+			dataset.tData.push_back ( std::vector<std::vector<double>> ( ) );
+
+			for ( int timePoint = 0; timePoint < filePointLength[filePoint]; timePoint++ )
+			{
+				dataset.tData[filePoint].push_back ( std::vector<double> ( reducedDimensionCount, 0.0 ) );
+
+				fluid::RealVector pointVals ( reducedDimensionCount );
+				fluidset.get ( std::to_string ( fileMarker + timePoint ), pointVals );
+
+				for ( int dimension = 0; dimension < reducedDimensionCount; dimension++ )
+				{
+					dataset.tData[filePoint][timePoint][dimension] = pointVals[dimension];
+				}
+			}
+
+			fileMarker += filePointLength[filePoint];
+		}
+	}
+	else
+	{
+		dataset.sData.clear ( );
+		dataset.sDataReduced.clear ( );
+
+		for ( int filePoint = 0; filePoint < fluidset.size ( ); filePoint++ )
+		{
+			dataset.sDataReduced.push_back ( std::vector<double> ( reducedDimensionCount, 0.0 ) );
+
+			fluid::RealVector pointVals ( reducedDimensionCount );
+			fluidset.get ( std::to_string ( filePoint ), pointVals );
+
+			for ( int dimension = 0; dimension < reducedDimensionCount; dimension++ )
+			{
+				dataset.sDataReduced[filePoint][dimension] = pointVals[dimension];
+			}
+		}
+	}
+
 }
