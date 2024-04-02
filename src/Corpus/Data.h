4edd3dba923a4906ecdda8c0e848ff5ddabd86dc
Elowyn Fearne <gitfearne@proton.me>
2024-04-02 14:53:37 +0100

    default constructors for datastruct

--- src/Corpus/Data.h
+++ src/Corpus/Data.h
@@ -23,28 +23,28 @@ struct StatsData {
 };
 
 struct AnalysisSettings {
-	int currentDimensionCount;
-	bool hasBeenReduced;
-	bool bTime;
-	bool bPitch;
-	bool bLoudness;
-	bool bShape;
-	bool bMFCC;
-	int windowFFTSize;
-	int hopFraction;
-	int nBands;
-	int nCoefs;
-	int minFreq;
-	int maxFreq;
+	int currentDimensionCount = 0;
+	bool hasBeenReduced = false;
+	bool bTime = false;
+	bool bPitch = false;
+	bool bLoudness = false;
+	bool bShape = false;
+	bool bMFCC = false;
+	int windowFFTSize = 1024;
+	int hopFraction = 2;
+	int nBands = 40;
+	int nCoefs = 13;
+	int minFreq = 20;
+	int maxFreq = 5000;
 };
 
 struct ReductionSettings {
-	int dimensionReductionTarget;
-	int maxIterations;
+	int dimensionReductionTarget = 3;
+	int maxIterations = 200;
 };
 
 struct DataSet {
-	int currentPointCount;
+	int currentPointCount = 0;
 
 	std::vector<std::string> dimensionNames; // [dimension]
 	std::vector<std::string> fileList; // [file]
