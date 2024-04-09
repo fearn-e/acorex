0abbe7d9500be06c8f69e4a0d4bada98b34c9038
Elowyn Fearne <gitfearne@proton.me>
2024-04-09 01:08:25 +0100

    ExplorerMenu class

---
+++ src/ExplorerMenu.cpp
@@ -0,0 +1,33 @@
+#include "./ExplorerMenu.h"
+
+using namespace Acorex;
+
+void ExplorerMenu::Show ( )
+{
+}
+
+void ExplorerMenu::Reset ( )
+{
+}
+
+void ExplorerMenu::Draw ( )
+{
+}
+
+void ExplorerMenu::Exit ( )
+{
+}
+
+void ExplorerMenu::RemoveListeners ( )
+{
+}
+
+// Main Functions ------------------------------
+
+void ExplorerMenu::OpenCorpus ( )
+{
+}
+
+void ExplorerMenu::SwapDimension ( )
+{
+}
