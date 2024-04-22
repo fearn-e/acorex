d5ec41b0b8e21e1e1cec84da9ba0d831a4a0b69f
Elowyn Fearne <gitfearne@proton.me>
2024-04-22 18:07:49 +0100

    comments

--- src/ofApp.h
+++ src/ofApp.h
@@ -18,15 +18,7 @@ public:
 
 	//void audioIn ( float* input, int bufferSize, int nChannels );
 	//void audioOut ( float* buffer, int bufferSize, int nChannels );
-
-	//void keyPressed ( int key );
-	//void keyReleased ( int key );
-	//void mouseMoved ( int x, int y );
-	//void mouseDragged ( int x, int y, int button );
-	//void mousePressed ( int x, int y, int button );
-	//void mouseReleased ( int x, int y, int button );
-	//void mouseEntered ( int x, int y );
-	//void mouseExited ( int x, int y );
+	
 	//void windowResized ( int w, int h );
 	//void dragEvent ( ofDragInfo dragInfo );
 	//void gotMessage ( ofMessage msg );
