ef379aaee7c17607827bbd03a09866682349261a
Elowyn Fearne <gitfearne@proton.me>
2024-04-25 21:35:14 +0100

    value tweaks

--- src/Explorer/SpaceDefs.h
+++ src/Explorer/SpaceDefs.h
@@ -8,13 +8,13 @@ namespace SpaceDefs {
 	// Camera Adjustments --------------------
 
 	static float mCamZoomSpeed3D = 200;		static float mCamZoomSpeed2D = 0.1;
-	static float mZoomMin3D = 50.0;			static float mZoomMax3D = 5000.0;
+	static float mZoomMin3D = 50.0;			static float mZoomMax3D = 10000.0;
 	static float mZoomMin2D = 0.1;			static float mZoomMax2D = 10.0;
 	static float mCamMoveSpeed = 1.0;
 	static float mCamRotateSpeed = 0.006;
 	static float mKeyboardMoveSpeed = 3000.0;
-	static float mKeyboardRotateSpeed = 1500.0;
-	static float mKeyboardZoomSpeed = 750.0;
+	static float mKeyboardRotateSpeed = 500.0;
+	static float mKeyboardZoomSpeed = 50.0;
 
 	// Space and Color Limits --------------------
 
