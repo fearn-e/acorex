260d1638a2aec6b15df482c23a3eaf8aaac8117d
Elowyn Fearne <gitfearne@proton.me>
2024-08-09 14:52:39 +0100

    move audio file loading into new utils class

--- src/Analyser/GenAnalysis.h
+++ src/Analyser/GenAnalysis.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Utils/Data.h"
+#include "Utils/AudioFileLoader.h"
 #include <Eigen/Core>
 #include <algorithms/public/DCT.hpp>
 #include <algorithms/public/Loudness.hpp>
@@ -9,7 +10,6 @@
 #include <algorithms/public/STFT.hpp>
 #include <algorithms/public/SpectralShape.hpp>
 #include <algorithms/public/YINFFT.hpp>
-#include <audio_file/in_file.hpp>
 #include <data/FluidDataSet.hpp>
 #include <data/FluidIndex.hpp>
 #include <data/FluidJSON.hpp>
@@ -30,14 +30,11 @@ public:
 	int ProcessFiles ( Utils::DataSet& dataset );
 
 private:
-	bool ReadFile ( std::string filename, fluid::RealVector& output, double& sampleRate );
-
-	void ReadToMono ( fluid::RealVector& output, htl::in_audio_file& file );
-	void ReadToMono ( fluid::RealVector& output, ofxAudioFile& file );
-
 	fluid::RealVector ComputeStats ( fluid::RealMatrixView matrix, fluid::algorithm::MultiStats stats );
 
 	void Push7Stats ( fluid::RealVector& stats, std::vector<std::vector<double>>& fileData, int numDimensions );
+
+	Utils::AudioFileLoader mAudioLoader;
 };
 
 } // namespace Analyser
