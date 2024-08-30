5b46c7e602cf3935c7a7dca27c04f00084c3e7a9
Elowyn Fearne <gitfearne@proton.me>
2024-08-30 19:16:37 +0100

    pointpicker check enough samples left in target file

--- src/Explorer/AudioPlayback.cpp
+++ src/Explorer/AudioPlayback.cpp
@@ -181,12 +181,15 @@ void Explorer::AudioPlayback::audioOut ( ofSoundBuffer& outBuffer )
 			{
 				std::lock_guard<std::mutex> lock ( mTimeCorpusMutex, std::adopt_lock );
 
-				size_t timePointIndex = mPlayheads[playheadIndex].sampleIndex / (mRawView->GetDataset ( )->analysisSettings.windowFFTSize / mRawView->GetDataset ( )->analysisSettings.hopFraction);
+				size_t hopSize = mRawView->GetDataset ( )->analysisSettings.windowFFTSize / mRawView->GetDataset ( )->analysisSettings.hopFraction;
+				size_t timePointIndex = mPlayheads[playheadIndex].sampleIndex / hopSize;
 				glm::vec3 playheadPosition = mTimeCorpus[mPlayheads[playheadIndex].fileIndex].getVertex ( timePointIndex );
 				Utils::PointFT nearestPoint;
 				Utils::PointFT currentPoint; currentPoint.file = mPlayheads[playheadIndex].fileIndex; currentPoint.time = timePointIndex;
 
-				if ( !mPointPicker->FindNearestToPosition ( playheadPosition, nearestPoint, currentPoint, mMaxJumpDistanceSpaceX1000, mMaxJumpTargets, mJumpSameFileAllowed, mJumpSameFileMinTimeDiff ) )
+				if ( !mPointPicker->FindNearestToPosition ( playheadPosition, nearestPoint, currentPoint, 
+															mMaxJumpDistanceSpaceX1000, mMaxJumpTargets, mJumpSameFileAllowed, 
+															mJumpSameFileMinTimeDiff, requiredSamples, *mRawView->GetAudioData ( ), hopSize ) )
 				{ continue; }
 
 				jumpNext = true;
