8cd4b1d77698b72a9eedb8771e6de3c5921b856c
fearne <gitfearne@proton.me>
2024-09-03 17:56:36 +0100

    missing include

--- src/Analyser/GenAnalysis.cpp
+++ src/Analyser/GenAnalysis.cpp
@@ -16,6 +16,7 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 
 #include "./GenAnalysis.h"
 #include <ofLog.h>
+#include <ofUtils.h>
 
 #if __has_include(<omp.h>)
 #include <omp.h>
