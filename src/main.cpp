752eabf5184c8068c8dd889f37844539589e17c7
fearne <gitfearne@proton.me>
2025-02-17 20:26:40 +0000

    window title

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 int main ()
 {
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
-	InitWindow(1920, 1080, "Hello Raylib");
+	InitWindow(1920, 1080, "ACorEx");
 	SetExitKey(KEY_NULL);
 	
 	// main loop
