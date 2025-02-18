eb1c45c136def725bb6a5f43e1cfe03c421f4cef
fearne <gitfearne@proton.me>
2025-02-18 17:42:53 +0000

    RawView audio loading

--- src/Utils/AudioFileLoader.h
+++ src/Utils/AudioFileLoader.h
@@ -17,7 +17,6 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 #pragma once
 
 #include <raylib.h>
-#include <data/TensorTypes.hpp>
 #include <string>
 
 namespace Acorex {
@@ -28,7 +27,7 @@ public:
 	AudioFileLoader ( ) { }
 	~AudioFileLoader ( ) { }
 
-	bool ReadAudioFile ( std::string filename, fluid::RealVector& output, double targetSampleRate );
+	bool ReadAudioFile ( std::string filename, std::vector<float>& output, double targetSampleRate );
 
 private:
 	//void ReadToMono ( std::vector<float>& output, htl::in_audio_file& file );
