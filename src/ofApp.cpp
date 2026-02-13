71ebb71baddfbe55f0d97758c230a480683aea69
fearn-e <gitfearne@proton.me>
2026-02-13 15:14:05 +0000

    constructor initializer

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -18,10 +18,7 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 
 #include "Utils/TemporaryDefaults.h"
 
-ofApp::ofApp ( )
-{
-    bListenersAdded = false;
-}
+ofApp::ofApp ( ) : bListenersAdded ( false ) { }
 
 void ofApp::setup ( )
 {   
