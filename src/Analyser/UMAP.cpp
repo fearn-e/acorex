3fa437d4585e1c71081808441898d5730b092d27
fearn-e <gitfearne@proton.me>
2026-09-14 20:31:11 +0100

    external includes ignore some warnings when compiling

--- src/Analyser/UMAP.cpp
+++ src/Analyser/UMAP.cpp
@@ -16,7 +16,7 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 
 #include "Analyser/UMAP.h"
 
-#include "ofLog.h"
+#include <ofLog.h>
 
 using namespace Acorex;
 
--- src/Explorer/AudioPlayback.cpp
+++ src/Explorer/AudioPlayback.cpp
@@ -16,7 +16,7 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 
 #include "Explorer/AudioPlayback.h"
 
-#include "ofLog.h"
+#include <ofLog.h>
 #include <random>
 #include <optional>
 
