e0f1a37c78a383a1c46dfa2feb019922ad58a45d
fearn-e <gitfearne@proton.me>
2026-09-18 19:56:48 +0100

    not yet functional - import Data.h as header unit attempt + changed to compilable in properties

--- src/Explorer/LiveView.h
+++ src/Explorer/LiveView.h
@@ -20,13 +20,14 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 #include "Explorer/PointPicker.h"
 #include "Explorer/RawView.h"
 #include "Explorer/AudioPlayback.h"
-#include "Utilities/Data.h"
 #include "Utilities/DimensionBounds.h"
 #include "Utilities/InterfaceDefs.h"
 
 #include <ofMesh.h>
 #include <ofEasyCam.h>
 #include <random>
+
+import "Utilities/Data.h";
 
 namespace Acorex {
 namespace Explorer {
